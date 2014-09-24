c20e7d5604f24eae30537379b637e8588e100161
Sam Glover <sam.glover11@gmail.com>
2014-09-24 15:01:18 -0400

    testing before_filer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :protect
+
+def protect
+  @ips = ['127.0.0.1', '203.123.10.1'] #And so on ...]
+  if not @ips.include? request.remote_ip
+     if user = authenticate_with_http_basic { |u, p| u=='sbx' and p=='sbx' }
+          @current_user = user
+     else
+          request_http_basic_authentication
+     end
+  end
+end
+
 end
